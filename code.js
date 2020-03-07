1b5d9bf5a12e29c4b598cf928dab788e75023fd9
Feneel29 <55814045+Feneel29@users.noreply.github.com>
2020-03-07 18:40:18 -0500

    Update code.js

--- code.js
+++ code.js
@@ -4,7 +4,7 @@ const yelp = require('yelp-fusion');
 
 // Place holder for Yelp Fusion's API Key. Grab them
 // from https://www.yelp.com/developers/v3/manage_app
-const apiKey = '<YOUR_API_KEY>';
+const apiKey = '_fRGx0tK92V-Bzxhhtm-hepIOprXRHT97KK0XOkVHgTGiLDtZy4RYweKOGZmlNqD3OJ_9HpESDi9sJ7EiHoGSZKySR0ITPwUCpT8e3yojKJEYTjROtl2VzZg3OdZXnYx';      
 
 const searchRequest = {
   term:'Four Barrel Coffee',
@@ -19,4 +19,6 @@ client.search(searchRequest).then(response => {
   console.log(prettyJson);
 }).catch(e => {
   console.log(e);
-});
+});-'
+
+
